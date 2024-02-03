171e36612e8a767d1bd74dd4f6eb9c86cc1f8de9
Sh4dowX404Unknown <158760312+Sh4dowX404Unknown@users.noreply.github.com>
2024-02-03 20:37:54 +0100

    Create automated-setup.sh

---
+++ automated-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is designed to move a Python script (whichsystem.py) to the /usr/local/bin directory
+# and give it executable permissions.
+
+# Move the Python script to /usr/local/bin
+sudo mv whichsystem.py /usr/local/bin
+
+# Give the script executable permissions
+sudo chmod +x /usr/local/bin/whichsystem.py
